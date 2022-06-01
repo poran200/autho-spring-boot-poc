a3f2333fe6075c6570c7462283a6c323f9e84c44
poran <poran.chowdury@selise.ch>
2022-06-01 14:38:49 +0600

    Initial commit

---
+++ src/main/java/ch/selise/authospringbootpoc/AuthoSpringBootPocApplication.java
@@ -0,0 +1,13 @@
+package ch.selise.authospringbootpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthoSpringBootPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthoSpringBootPocApplication.class, args);
+    }
+
+}
